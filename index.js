ef2abfc463fa0cf0b3517bbd1dabea0fcd59e205
klezi10 <74952593+klezi10@users.noreply.github.com>
2021-11-16 14:28:17 +0700

    cleaned code

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 let myEmojis = ['🌟', '😎', '🧁'];
 const emojiContainer = document.getElementById('emoji-container');
+const emojiInput = document.getElementById('emoji-input');
 const pushBtn = document.getElementById('push-btn');
+const unshiftBtn = document.getElementById('unshift-btn');
 
 function renderEmojis() {
+  emojiContainer.innerHTML = '';
   for (let i = 0; i < myEmojis.length; i++) {
     const emoji = document.createElement('span');
     emoji.textContent = myEmojis[i];
@@ -12,15 +15,18 @@ function renderEmojis() {
 
 renderEmojis();
 
-pushBtn.addEventListener('click', function (event) {
-  event.preventDefault();
-  const emojiInput = document.getElementById('emoji-input');
-  console.log(emojiInput.value);
-
+pushBtn.addEventListener('click', function () {
   if (emojiInput.value) {
     myEmojis.push(emojiInput.value);
     emojiInput.value = null;
-    emojiContainer.innerHTML = '';
+    renderEmojis();
+  }
+});
+
+unshiftBtn.addEventListener('click', function () {
+  if (emojiInput.value) {
+    myEmojis.unshift(emojiInput.value);
+    emojiInput.value = null;
     renderEmojis();
   }
 });
